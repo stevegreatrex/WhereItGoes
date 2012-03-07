8c4c63fab3f2b1204348c1dbbfec26fff7c4e927
stevegreatrex <greatrex696@hotmail.com>
2012-03-07 21:41:55 +0000

    Added knockout and a basic view model for home page

---
+++ WhereItGoes/WhereItGoes.Web/Scripts/App.js
@@ -0,0 +1,2 @@
+﻿App = {};
+App.ViewModels = {};
---
+++ WhereItGoes/WhereItGoes.Web/Scripts/ViewModels/HomeViewModel.js
@@ -0,0 +1,13 @@
+﻿(function (App) {
+    App.ViewModels.HomeViewModel = function () {
+        var _self = {};
+
+        _self.message = ko.observable("Find out where it goes...");
+
+        return _self;
+    };
+})(App);
+
+$(function () {
+    ko.applyBindings(new App.ViewModels.HomeViewModel());
+});
