2b4872b40bb6c82432ed31a4c3d82470c724ccee
stevegreatrex <greatrex696@hotmail.com>
2012-03-08 07:58:36 +0000

    Added qunit and initial test for editableviewmodelbase

---
+++ WhereItGoes/WhereItGoes.Web/Scripts/Tests/TestCategoriesViewModel.js
@@ -0,0 +1,8 @@
+﻿module("CategoriesViewModel Tests");
+
+test("Adds UI Flags", function () {
+    var vm = new App.ViewModels.EditableViewModelBase();
+    equal(vm.editing(), false, "Should not be editing");
+    equal(vm.saving(), false, "Should not be saving");
+    equal(vm.error(), false, "Should not have an error");
+});
