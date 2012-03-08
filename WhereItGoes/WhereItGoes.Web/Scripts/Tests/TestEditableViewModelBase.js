673a8e40ba5622e83402933bc34742768449c3a0
stevegreatrex <greatrex696@hotmail.com>
2012-03-08 09:11:57 +0000

    Added first unit test for CategoryViewModel

---
+++ WhereItGoes/WhereItGoes.Web/Scripts/Tests/TestEditableViewModelBase.js
@@ -0,0 +1,71 @@
+﻿module("EditableViewModelBase Tests");
+
+test("Adds UI Flags", function () {
+    var vm = new App.ViewModels.EditableViewModelBase();
+    equal(vm.editing(), false, "Should not be editing");
+    equal(vm.saving(), false, "Should not be saving");
+    equal(vm.error(), false, "Should not have an error");
+});
+
+test("Edit", function () {
+    var vm = new App.ViewModels.EditableViewModelBase();
+    vm.edit();
+    equal(vm.editing(), true, "Editing should now  be true");
+});
+
+test("Commit", function () {
+    var vm = new App.ViewModels.EditableViewModelBase();
+
+    //enter edit mode
+    vm.edit();
+    equal(vm.editing(), true, "Editing should now  be true");
+
+    //replace the inner commit method
+    var completeFunction = null;
+    vm._commit = function (complete) {
+        completeFunction = complete;
+    };
+
+    //start the commit
+    vm.commit();
+
+    //check that we are now saving and that the commit function has been called
+    equal(vm.editing(), false, "Should not now be editing");
+    equal(vm.saving(), true, "Should now be saving");
+    notEqual(completeFunction, null, "The inner complete function should have been called");
+
+    //now call the complete function with Success=true
+    completeFunction(true);
+    equal(vm.saving(), false, "Save should be completed");
+    equal(vm.error(), false, "No error should be reported");
+    equal(vm.editing(), false, "Should still not be editing");
+
+    //commit again, but this time report an error
+    vm.edit();
+    vm.commit();
+    completeFunction(false);
+    equal(vm.saving(), false, "Save should be completed, even though there was an error");
+    equal(vm.error(), true, "The error should be reported");
+    equal(vm.editing(), false, "Should still not be editing");
+});
+
+test("Cancel", function () {
+    var vm = new App.ViewModels.EditableViewModelBase();
+
+    //enter edit mode
+    vm.edit();
+    equal(vm.editing(), true, "Editing should now  be true");
+
+    //replace inner cancel
+    var cancelled = false;
+    vm._cancel = function () {
+        cancelled = true;
+    };
+
+    //now cancel
+    vm.cancel();
+
+    //check that editing was reset and that the inner cancel was called
+    equal(vm.editing(), false, "Editing should have been reset");
+    equal(cancelled, true, "The inner cancel method should have been executed");
+});
