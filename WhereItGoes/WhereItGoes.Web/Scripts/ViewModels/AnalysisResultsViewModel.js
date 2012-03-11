ff932b04bd1bcce44a7d9c4b9c36505879885788
stevegreatrex <greatrex696@hotmail.com>
2012-03-11 17:10:39 +0000

    Added some references and the initial file for analysis results view model to enable plotting of results

---
+++ WhereItGoes/WhereItGoes.Web/Scripts/ViewModels/AnalysisResultsViewModel.js
@@ -0,0 +1,21 @@
+﻿(function (App) {
+    App.ViewModels.AnalysisResultsViewModel = function (resultsDivId, data) {
+        var _self = {};
+
+        $.jqplot(resultsDivId, [data],
+        {
+            seriesDefaults: {
+                renderer: $.jqplot.PieRenderer,
+                rendererOptions: {
+                    showDataLabels: true
+                }
+            },
+            legend: {
+                show: true,
+                location: "e"
+            }
+        });
+
+        return _self;
+    };
+})(App);
