278041c6bf4c2548174457c5029645513fc75f51
stevegreatrex <greatrex696@hotmail.com>
2012-03-12 19:08:58 +0000

    Fixed folder structure

--- WhereItGoes.Web/Scripts/ViewModels/HomeViewModel.js
+++ WhereItGoes.Web/Scripts/ViewModels/HomeViewModel.js
@@ -3,6 +3,11 @@
         var _self = {};
         _self.message = ko.observable("Find out where it goes...");
 
+        var data = { from: new Date(1, 1, 2001), to: new Date() };
+        App.Utils.postJson("home/analyse", data, function (results) {
+            //alert(JSON.stringify(results));
+        });
+
         return _self;
     };
 })(App);
