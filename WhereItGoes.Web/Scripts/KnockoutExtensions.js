1f0ffda82953a95113efe185e057d613f4b64715
stevegreatrex <greatrex696@hotmail.com>
2012-03-28 20:22:43 +0100

    Added tablesorter to properly display table

---
+++ WhereItGoes.Web/Scripts/KnockoutExtensions.js
@@ -0,0 +1,8 @@
+﻿ko.bindingHandlers.tablesorter = {
+    init: function (element, valueAccessor) {
+        $(element).parent("table").tablesorter();
+    },
+    update: function (element, valueAccessor) {
+        $(element).parent("table").trigger("update");
+    }
+};
